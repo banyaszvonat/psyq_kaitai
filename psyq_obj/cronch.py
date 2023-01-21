426699b19dc4f10c625a773e3a738b373add2999
banyaszvonat <banyaszvonat@national.shitposting.agency>
2023-01-21 19:32:12 +0100

    Filename of the library to be cronched is now a commandline parameter

--- psyq_obj/cronch.py
+++ psyq_obj/cronch.py
@@ -1,11 +1,17 @@
 from parsers.psyq_lib import PsyqLib
 from obj_sym_extractor import ObjSymExtractor
 import os
+import sys
+
+if len(sys.argv) == 1:
+	print("Usage: cronch.py <library file>")
+	print("PROTIP: you'll want the library file to be in the current directory. Output directory is the first part of the filename split on '.'")
+	exit()
 
 extractor = ObjSymExtractor()
-psq_lib = PsyqLib.from_file("LIBMCRD.LIB")
+psq_lib = PsyqLib.from_file(sys.argv[1])
 
-dest_folder_path = os.path.join(os.getcwd(), "LIBMCRD.LIB".split(".")[0])
+dest_folder_path = os.path.join(os.getcwd(), sys.argv[1].split(".")[0])
 if not os.path.isdir(dest_folder_path):
 	os.mkdir(dest_folder_path)
 
